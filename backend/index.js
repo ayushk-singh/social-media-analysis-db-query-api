b1addafb1211f1b616f8e5198dc513d512b0824d
Ayush Kumar Singh <aayushsingh.code@gmail.com>
2025-01-02 12:01:28 +0530

    Restructured Project

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { Client } from "cassandra-driver";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-dotenv.config({})
+dotenv.config({});
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const client = new Client({
   cloud: {
@@ -21,25 +21,29 @@ client.connect();
 
 app.get("/aggregate", async (req, res) => {
   try {
-    const query = "SELECT type, likes, shares, comments FROM engagement.engagement;";
+    const query =
+      "SELECT type, likes, shares, comments FROM engagement.engagement;";
     const result = await client.execute(query);
-    
+
     // Check if result is empty
     if (result.rows.length === 0) {
       return res.status(404).send("No data found.");
     }
 
-    const averages = result.rows.reduce((acc, { type, likes, shares, comments }) => {
-      if (!acc[type]) {
-        acc[type] = { likes: 0, shares: 0, comments: 0, count: 0 };
-      }
-      acc[type].likes += likes;
-      acc[type].shares += shares;
-      acc[type].comments += comments;
-      acc[type].count += 1;
-  
-      return acc;
-    }, {});
+    const averages = result.rows.reduce(
+      (acc, { type, likes, shares, comments }) => {
+        if (!acc[type]) {
+          acc[type] = { likes: 0, shares: 0, comments: 0, count: 0 };
+        }
+        acc[type].likes += likes;
+        acc[type].shares += shares;
+        acc[type].comments += comments;
+        acc[type].count += 1;
+
+        return acc;
+      },
+      {}
+    );
 
     // Calculate average for each type
     const resultData = [];
@@ -60,5 +64,7 @@ app.get("/aggregate", async (req, res) => {
     res.status(500).send("Error retrieving data from the database.");
   }
 });
+
+// running server
 
 app.listen(port, () => console.log(`API running on http://localhost:${port}`));
